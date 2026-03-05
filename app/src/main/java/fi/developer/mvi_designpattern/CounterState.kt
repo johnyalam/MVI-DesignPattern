7a06ef58cc3eb6b61e49a71f19df94a0d213229f
Johny Alam <johny.alam@hublet.fi>
2026-03-05 11:02:50 +0200

    Counter reducer added.

--- app/src/main/java/fi/developer/mvi_designpattern/CounterState.kt
+++ app/src/main/java/fi/developer/mvi_designpattern/CounterState.kt
@@ -1,3 +1,7 @@
 package fi.developer.mvi_designpattern
 
-data class CounterState (val count: Int = 0)
+data class CounterState (
+    val count: Int = 0,
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
